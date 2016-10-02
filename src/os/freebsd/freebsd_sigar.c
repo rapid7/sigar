d76b88e01c641d5fe42ad9b1cfc05f53f52309c1
Brent Cook <busterb@gmail.com>
2016-10-02 12:06:09 -0500

    fix freebsd builds

--- src/os/freebsd/freebsd_sigar.c
+++ src/os/freebsd/freebsd_sigar.c
@@ -405,8 +405,6 @@ static int sigar_vmstat(sigar_t *sigar, struct vmmeter *vmstat)
     GET_VM_STATS(vm, v_inactive_target, 0);
     GET_VM_STATS(vm, v_inactive_count, 1);
     GET_VM_STATS(vm, v_cache_count, 1);
-    GET_VM_STATS(vm, v_cache_min, 0);
-    GET_VM_STATS(vm, v_cache_max, 0);
     GET_VM_STATS(vm, v_pageout_free_min, 0);
     GET_VM_STATS(vm, v_interrupt_free_min, 0);
     GET_VM_STATS(vm, v_forks, 0);

4e41392ab87fa01215aa5936e2cfa183e883680b
Brent Cook <busterb@gmail.com>
2020-06-08 09:54:11 -0500

    leave room at the end of buf for a null terminator

--- src/sigar_util.c
+++ src/sigar_util.c
@@ -924,7 +924,7 @@ int sigar_file2str(const char *fname, char *buffer, int buflen)
         return ENOENT;
     }
 
-    if ((len = read(fd, buffer, buflen)) < 0) {
+    if ((len = read(fd, buffer, buflen - 1)) < 0) {
         status = errno;
     } else {
         status = SIGAR_OK;
